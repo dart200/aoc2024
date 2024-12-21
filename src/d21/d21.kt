ea26e9f0b5e8d1b3f632edfe127af1426a236059
Nick Swenson <dart200@gmail.com>
2024-12-21 13:56:02 -0800

    d21 part 2

--- src/d21/d21.kt
+++ src/d21/d21.kt
@@ -2,38 +2,27 @@ package d21
 
 import java.io.File
 import kotlin.math.abs
-import kotlin.time.measureTimedValue
+import kotlin.time.measureTime
 
 fun numpadMap (num: Char) = when(num) {
-    '7' -> (0 to 0)
-    '8' -> (0 to 1)
-    '9' -> (0 to 2)
-    '4' -> (1 to 0)
-    '5' -> (1 to 1)
-    '6' -> (1 to 2)
-    '1' -> (2 to 0)
-    '2' -> (2 to 1)
-    '3' -> (2 to 2)
-    '0' -> (3 to 1)
-    'A' -> (3 to 2)
+    '7' -> (0 to 0); '8' -> (0 to 1); '9' -> (0 to 2)
+    '4' -> (1 to 0); '5' -> (1 to 1); '6' -> (1 to 2)
+    '1' -> (2 to 0); '2' -> (2 to 1); '3' -> (2 to 2)
+                     '0' -> (3 to 1); 'A' -> (3 to 2)
     else -> throw Exception("no")
 }
 
 fun arrowpadMap (ch:Char) = when (ch) {
-    '^' -> (0 to 1)
-    'A' -> (0 to 2)
-    '<' -> (1 to 0)
-    'v' -> (1 to 1)
-    '>' -> (1 to 2)
+                     '^' -> (0 to 1); 'A' -> (0 to 2)
+    '<' -> (1 to 0); 'v' -> (1 to 1); '>' -> (1 to 2)
     else -> throw Exception("no")
 }
 
-val DEPTH=19
+val DEPTH=26
 
-val cacheMove = HashMap<Triple<Char,Char,Int>,String>()
-fun genMove (startChr:Char, endChr:Char, depth:Int):String
-    = cacheMove.getOrPut(Triple(startChr,endChr,depth))
-    {
+val moveCache = HashMap<Int,Long>()
+fun genMove (startChr:Char, endChr:Char, depth:Int):Long
+    = moveCache.getOrPut(depth shl 16 xor endChr.code shl 8 xor startChr.code) {
         val map = if (depth==DEPTH) ::numpadMap else ::arrowpadMap
 
         var (sr,sc)=map(startChr); var (er,ec)=map(endChr)
@@ -44,7 +33,7 @@ fun genMove (startChr:Char, endChr:Char, depth:Int):String
         val rFirst = ir+ic+"A"
         val cFirst = ic+ir+"A"
 
-        if (map==::numpadMap) {
+        if (map==::numpadMap) { // avoid space
             if (sc==0 && er==3) return@getOrPut genMoves(cFirst,depth-1)
             if (sr==3 && ec==0) return@getOrPut genMoves(rFirst,depth-1)
         } else if (map==::arrowpadMap){
@@ -52,30 +41,21 @@ fun genMove (startChr:Char, endChr:Char, depth:Int):String
             if (sr==0 && ec==0) return@getOrPut genMoves(rFirst,depth-1)
         }
 
-        val rfirstGen = genMoves(ir+ic+"A",depth-1)
-        val cFirstGen = genMoves(ic+ir+"A",depth-1)
-        return@getOrPut if (rfirstGen.length <= cFirstGen.length) rfirstGen else cFirstGen
+        val rfirstGen = genMoves(rFirst,depth-1)
+        val cFirstGen = genMoves(cFirst,depth-1)
+        return@getOrPut if (rfirstGen <= cFirstGen) rfirstGen else cFirstGen
     }
 
-//val cacheMoves = HashMap<Pair<String,Int>,String>()
-fun genMoves (output:String, depth:Int) =
-//    cacheMoves.getOrPut(Pair(output,depth)) {
-        if (depth == 0)
-            output
-        else
-            ("A$output").zipWithNext().fold("") { instr, (prevChr, chr) ->
-                instr + genMove(prevChr, chr, depth)
-            }
-//    }
+fun genMoves (output:String, depth:Int):Long =
+        if (depth == 0) output.length.toLong()
+        else ("A$output").zipWithNext().fold(0L) { instr, (prevChr, chr) ->
+            instr + genMove(prevChr, chr, depth)
+        }
 
 fun main () {
-    var complexity = File("src/d21/d21.in").readLines().sumOf {
-        var instr = measureTimedValue { genMoves(it, DEPTH) }
-            .let { (v, t) -> println(t); v }
-//        cacheMoves.clear()
-        var keypadNum = Regex("[0-9]+").find(it)?.value?.toLong()!!
-        println("${instr.length},$keypadNum")
-        instr.length * keypadNum
-    }
-    println(complexity)
+    println(measureTime{
+        println(File("src/d21/d21.in").readLines().sumOf {
+            genMoves(it, DEPTH) * Regex("[0-9]+").find(it)?.value?.toLong()!!
+        })
+    })
 }
