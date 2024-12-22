4807d3e884f292dc13365fdc4670b1d13b98b3d0
Nick Swenson <dart200@gmail.com>
2024-12-21 23:56:44 -0800

    d22

--- src/d22/d22.kt
+++ src/d22/d22.kt
@@ -6,42 +6,48 @@ import kotlin.time.measureTime
 
 const val MOD = 16777216L
 fun genNext (num:Long):Long {
-    val s1 = ((num shl 6) xor num).mod(MOD)
-    val s2 = ((s1 shr 5) xor s1).mod(MOD)
-    return ((s2 shl 11) xor s2).mod(MOD)
+    val s1 = (num shl 6 xor num).mod(MOD)
+    val s2 = (s1 shr 5 xor s1).mod(MOD)
+    return (s2 shl 11 xor s2).mod(MOD)
 }
 
 fun getNth (num:Long, n:Int)
     = (0..<n).fold(num) {nth,_ -> genNext(nth)}
 fun q1 () {
-    println(File("src/d22/d22.in").readLines().sumOf { l ->
-        getNth(l.trim().toLong(), 2000)
-    })
+    println(File("src/d22/d22.in").readLines()
+        .sumOf { getNth(it.trim().toLong(), 2000) })
 }
 
-val totals = HashMap<Int, Long>()
-fun doBuyer (num:Long, n:Int):Long {
-    val seenKeys = HashSet<Int>()
+val totals = Array<Int>(65536) {0}
+fun doBuyer (num:Long, n:Int) {
+    val seenKeys = Array<Boolean>(65536) {false}
     var cur = num; var prev = num; var curKey = 0
+
     for (i in 0..<n) {
         prev = cur
         cur = genNext(cur)
         val prevPrice = prev.mod(10)
         val curPrice = cur.mod(10)
         val dPrice = curPrice-prevPrice
-        curKey = (curKey shl 4) or (dPrice and 15) and 65535
-        if ( i >= 3 && !seenKeys.contains(curKey)) {
-            totals[curKey] = totals.getOrPut(curKey) {0L} + curPrice
-            seenKeys.add(curKey)
+
+        curKey = (curKey shl 4) or (dPrice and 0b1111) and 65535
+
+        if (i >= 3 && !seenKeys[curKey]) {
+            totals[curKey] += curPrice
+            seenKeys[curKey] = true
         }
     }
-    return cur
 }
 fun q2 () {
-    File("src/d22/d22.in").readLines().forEach {doBuyer(it.trim().toLong(), 2000)}
-    println(totals.entries.maxByOrNull { it.value })
+    File("src/d22/d22.in").readLines()
+        .forEach { doBuyer(it.trim().toLong(), 2000) }
+    println(totals.maxByOrNull {it})
 }
 
+fun main () {
+    println(measureTime { q1() })
+    println(measureTime { q2() })
+}
 
 //fun getKeyList (key:Int) =
 //    (0..<4).reversed().map{i -> key.shr(i*8).toByte()}
@@ -63,9 +69,3 @@ fun q2 () {
 //    println(getKeyList(maxEnt.key))
 //    println(totals.entries.maxByOrNull { it.value })
 //}
-
-fun main () {
-//    qtest2()
-    q1()
-    println(measureTime { q2() })
-}
