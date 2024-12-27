388a94bcde5f1a441523cf3f8ca8136c850ec9c8
Nick Swenson <dart200@gmail.com>
2024-12-27 02:46:44 -0800

    d09 part 2

--- src/d09/d09.kt
+++ src/d09/d09.kt
@@ -1,31 +1,8 @@
 package d09
 
 import java.io.File
-import java.util.PriorityQueue
 import kotlin.time.measureTime
 
-//class INode (
-//    var idx:Int,
-//    var size:Int,
-//    var id:Int,
-//    var prev: INode?,
-//    var next: INode?
-//) {
-//
-//}
-//
-//fun buildList () {
-//    val nums = File("input/d09.in.test").readText().toCharArray().map { it.digitToInt() }
-//    var idx = 0; var head:INode?=null; var tail:INode?=null
-//    for ((i,size) in nums.withIndex()) {
-//        val node = INode(idx,size, if (i%2==0) i else -1, tail, null);
-//        if (head==null) head = node;
-//        if (tail!=null) tail.prev=node;
-//        tail=node
-//    }
-//
-//}
-//
 fun q1 () {
     val nums = File("src/d09/d09.in").readText().toCharArray().map { it.digitToInt() }.toMutableList()
 
@@ -77,79 +54,39 @@ fun q1 () {
 }
 
 fun q2 () {
-    val nums = File("src/d09/d09.in").readText().toCharArray().map { it.digitToInt() }.toMutableList()
-    // add files to set of 10 prio queues to figure what can be moved as iterate
-    // over spaces
-    val spaceQ = Array(10) {PriorityQueue<Int>(1000)}
-    for (i in nums.indices.reversed()) {
-        // add spaces to prio qs
-        if (i%2 == 1) {
-            var sz = nums[i]
-            spaceQ[sz].add(i)
-        }
+    data class Node (var idx:Int, var sz: Byte)
+    var diskIdx = 0
+    val nodes = File("src/d09/d09.in").readText().trim().toCharArray().map {
+        Node(diskIdx,it.digitToInt().toByte()
+            .also {diskIdx += it}
+        )
     }
+    val freeSpaceArr = nodes.filterIndexed {i,node -> i%2 == 1 && node.sz > 0}
 
-    // maps space idx -> files moves there
-    val moves = HashMap<Int, ArrayList<Int>>()
-    // contains idx of files that have been moved
-    val filesMoved = HashSet<Int>()
-    for (i in nums.indices.reversed()) {
-        if (i%2 == 0) {
-            val moveSz = nums[i]
-            val spaceSz = spaceQ.indices
-                .filter {spaceQ[it].isNotEmpty()}
-                .filter {it >= moveSz}
-                .minByOrNull {spaceQ[it].peek()}
-            if (spaceSz == null) continue
-
-            val toIdx = spaceQ[spaceSz].remove()
-            moves.getOrPut(toIdx) {arrayListOf()}.addLast(i)
-            filesMoved.add(i)
-
-            // reinsert leftover space
-            val spaceLeft = spaceSz-moveSz
-            if (spaceLeft > 0)
-                spaceQ[spaceLeft].add(toIdx)
-        }
-    }
-
-    var diskIndex = 0L
     var checkSum = 0L
-    fun doCheckSum(fileID:Int, sz:Int) =
-        (0..<sz).forEach {checkSum += fileID.toLong()*diskIndex; diskIndex += 1}
-    fun doSpace(sz:Int) =
-        (0..<sz).forEach {diskIndex += 1}
-
-    for (i in nums.indices) {
-        // files
-        if (i%2 == 0) {
-            if (filesMoved.contains(i)) {
-                doSpace(nums[i])
-            } else {
-                doCheckSum(i/2,nums[i])
-            }
-        // spaces
-        } else {
-            var spaceSz = nums[i]
-            val movedArr = moves[i]
-            movedArr?.forEach {fromIdx ->
-                val moveSz = nums[fromIdx]
-                doCheckSum(fromIdx/2,moveSz)
-                spaceSz -= moveSz
-            }
-            doSpace(spaceSz)
+    fun doCheckSum(fileID:Long, diskIdx:Int, sz:Byte) {
+        checkSum += (0..<sz).sumOf {
+            (fileID) * (diskIdx+it)
         }
     }
 
-    println()
+    for ((fileIdx,node) in nodes.withIndex().reversed()) {
+        if (fileIdx.mod(2) == 0) {
+            val freeSpc = freeSpaceArr.find {
+                it.sz >= node.sz && it.idx < node.idx
+            }
+            if (freeSpc == null) {
+                doCheckSum((fileIdx/2).toLong(),node.idx,node.sz)
+            } else {
+                doCheckSum((fileIdx/2).toLong(),freeSpc.idx,node.sz)
+                freeSpc.sz = freeSpc.sz.minus(node.sz).toByte()
+                freeSpc.idx += node.sz
+            }
+        }
+    }
+
     println(checkSum)
 }
-
-// s: 00...111...2...333.44.5555.6666.777.888899
-// e: 00992111777.44.333....5555.6666.....8888..
-
-// m: 00992111777.44.333....5555.6666.....8888..
-
 
 fun main () {
     println(measureTime {
@@ -157,3 +94,41 @@ fun main () {
         q2()
     })
 }
+
+//
+//    fun doSpace(sz:Byte) =
+//        (0..<sz).forEach {diskIdx += 1}
+//
+//    for ((i,nodeSz) in nodes.withIndex()) {
+//        var diskIdxPrev = diskIdx
+//        var szLeft = nodeSz - writes[i]?.sumOf {fromIdx ->
+//            val fileSz = nodes[fromIdx]
+//            doCheckSum(fromIdx.toLong(),fileSz)
+//            fileSz.toInt()
+//        }.let { it ?: 0 }
+//        diskIdx = diskIdxPrev + nodeSz
+//    }
+
+
+// 10 prio qs tracks idx of spaces for each size
+// val freeSpace = Array(10) {PriorityQueue<Int>()}
+//         //  freeSpace[nodeSz].add(i)
+
+//            val spaceSz = freeSpace.indices
+//                .filter {sz -> freeSpace[sz].isNotEmpty()}
+//                .filter {sz -> sz >= nodeSz}
+//                .minByOrNull {sz -> freeSpace[sz].peek()}
+//            if (spaceSz == null) {
+//                writes.put(fileIdx, arrayListOf(fileIdx))
+//            } else {
+//                val toIdx = freeSpace[spaceSz].remove()
+//                if (freeSpace.any {q -> q.contains(toIdx)}) throw Exception("FUCK")
+//                writes.getOrPut(toIdx) {arrayListOf()}.addLast(fileIdx)
+//
+//                // reinsert leftover space
+//                val szLeft = spaceSz-nodeSz
+//                if (szLeft > 0) {
+//                    freeSpace[szLeft].add(toIdx)
+//                    if (freeSpace.count { q -> q.contains(toIdx)} != 1) throw Exception("FUCK")
+//                }
+//            }
